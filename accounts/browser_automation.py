cd6954b1f174f1c681dd37969bea0eb2d9f2e448
UNKNWNCLOWNGHL <58516721+ciwie3303@users.noreply.github.com>
2024-04-07 01:45:53 +0400

    Update browser_automation.py

--- accounts/browser_automation.py
+++ accounts/browser_automation.py
@@ -8,6 +8,9 @@ from .models import Account
 
 
 def main(username):
+    log_output = ""  
+    logger.info("Запускаю эмуляцию!")
+    log_output += "Запускаю эмуляцию!\n"
     account = Account.objects.first()
     driver = webdriver.Chrome()
     driver.get("https://ru.stripchat.global/")
@@ -18,19 +21,18 @@ def main(username):
     time.sleep(10)
 
     if account:
-        # Используем email и password из базы данных для ввода в поля логина и пароля
+
         logger.success(f"Аккаунт найден - {account.email}:{account.password}")
         driver.find_element(By.ID, 'login_login_or_email').send_keys(account.email)
         driver.find_element(By.ID, 'login_password').send_keys(account.password)
     else:
-        logger.error("Нет доступных аккаунтов в базе данных.")
+        log_output += "Нет доступных аккаунтов в базе данных.\n"  
 
     api_key = '9fe3a62e9d89f6d358dbaa3facc553c7'
     solver = TwoCaptcha(api_key)
     response = solver.recaptcha(sitekey='6LdERdoaAAAAAD5YwFuKYsGz6VWgo6tC2ZMtQVmg', url="https://ru.stripchat.global/")
     logger.info('Капча решена: ' + str(response))
     recaptcha_response = response["code"]
-
 
     symbols = 'QWERTYUIOPASDFGHJKLZXCVBNM'
     for symbol in symbols:
@@ -40,20 +42,22 @@ def main(username):
                 driver.execute_script(
                     f"___grecaptcha_cfg.clients['0']['{symbol}']['{symbol}']['callback']('{recaptcha_response}')")
                 logger.success(f"Сегодня буква {symbol}")
+                log_output += f"Сегодня буква {symbol}\n"  
                 break
-
         except Exception:
             pass
-           
+
     driver.find_element(By.XPATH, '//*[@id="body"]/div[2]/div[1]/div/div[2]/div/form/div[4]/button').click()
     logger.success("Успешно зашел на аккаунт")
+    log_output += "Успешно зашел на аккаунт\n"  
     time.sleep(4)
     driver.find_element(By.XPATH, '//*[@id="body"]/div[2]/div/div[3]/div[2]/nav[2]/div').click()
     username_input = driver.find_element(By.XPATH, '//*[@id="body"]/div[2]/div/div[3]/div[2]/nav[2]/div/div/div[1]/input')
     username_input.send_keys(username)
     username_input.send_keys(Keys.ENTER)
     logger.info(f"Зашел на аккаунт {username}")
-    time.sleep(60)
+    log_output += f"Зашел на аккаунт {username}\n"  
+    time.sleep(20)
     driver.quit()
 
-
+    return log_output  
