39bd1590d06c9000962c82a372a039c6c74c8dd0
UNKNWNCLOWNGHL <58516721+ciwie3303@users.noreply.github.com>
2024-04-07 01:47:39 +0400

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 from .browser_automation import main
+import json
 
 def index(request):
-    if 'username' in request.POST:
-        username = request.POST['username']
-        main(username)
-    else:
-        print("Username отсутствует")
-    
+    return render(request, 'index.html')
 
-    return render(request, '../templates/index.html')
+def run_main(request):
+    if request.method == 'POST':
+        if 'username' in request.POST:
+            username = request.POST['username']
+            output_json = main(username)
 
+            output_text = json.dumps(output_json, ensure_ascii=False).encode('utf-8')
 
-
+            return JsonResponse({'output': output_text.decode('utf-8')}, charset='utf-8')
+    return JsonResponse({'error': 'Ошибка запроса'}, charset='utf-8')
